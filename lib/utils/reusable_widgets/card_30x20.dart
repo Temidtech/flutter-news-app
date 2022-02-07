13ed275a022c6c9c6fcb5b06faa545bf64c07d05
fortune2009 <fortunekbz2009@gmail.com>
2022-02-07 16:37:29 +0100

    commit for news ui

---
+++ lib/utils/reusable_widgets/card_30x20.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+class Card30x20 extends StatelessWidget {
+  Card30x20(
+      {Key? key,
+      this.source = "Time",
+      this.date = "February 2021",
+      this.tagColor = Colors.blueGrey,
+      this.title ='Title',
+      this.description = 'Description sfgsdfgsdfgdsfsdfgsffdsgdg',
+        this.image = "https://picsum.photos/200/300"})
+      : super(key: key);
+  final source;
+  final date;
+  final tagColor;
+  final title;
+  final description;
+  final image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          padding: EdgeInsets.all(8),
+          height: 170,
+          width: 120,
+          decoration: BoxDecoration(
+              color: Colors.blueGrey,
+              shape: BoxShape.rectangle,
+              image: DecorationImage(
+                  image: NetworkImage(image),
+              fit: BoxFit.fill,
+              ),
+              borderRadius: BorderRadius.circular(15)),
+          child: Stack(
+            children: [
+              Positioned(
+                child: Container(
+                  padding: EdgeInsets.all(3),
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.3)
+                  ),
+                  child: Text(title,
+                    style: TextStyle(
+                        color: Colors.white,
+                        ),
+                  ),
+                ),
+                top: 60,
+              ),
+              Positioned(
+                child: Container(
+                  width: 100,
+                    padding: EdgeInsets.all(3),
+                    decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.3)
+                    ),
+                    child: Text(description, maxLines:2, overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),)
+                ),
+                top: 110,
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Text(
+            date,
+            style: TextStyle(fontWeight: FontWeight.w600),
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Row(
+            children: [
+              Container(
+                height: 10,
+                width: 10,
+                decoration: BoxDecoration(
+                  color: tagColor,
+                  shape: BoxShape.rectangle,
+                ),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Text(source),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/views/home_screen.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/utils/reusable_widgets/card_30x20.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class News extends StatefulWidget {
+  const News({Key? key}) : super(key: key);
+
+  @override
+  _NewsState createState() => _NewsState();
+}
+
+class _NewsState extends State<News> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          "News+",
+          style: TextStyle(
+              color: Colors.black, fontSize: 21, fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search_rounded,
+                color: Colors.black,
+              ))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text("Featured",
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Container(
+              height: 240,
+              // height: double.maxFinite,
+              child: ListView.separated(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: 6,
+                itemBuilder: (context, index) => Card30x20(),
+                separatorBuilder: (BuildContext context, int index) {
+                  return SizedBox(width: 5);
+                },
+              ),
+            ),
+            SizedBox(
+              height: 35,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text("Discovery",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Container(
+              height: 240,
+              // height: double.maxFinite,
+              child: GridView.custom(
+                shrinkWrap: true,
+                gridDelegate: SliverWovenGridDelegate.count(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                  pattern: [
+                    WovenGridTile(1),
+                    WovenGridTile(
+                      5 / 7,
+                      crossAxisRatio: 0.9,
+                      alignment: AlignmentDirectional.centerEnd,
+                    ),
+                  ],
+                ),
+                childrenDelegate: SliverChildBuilderDelegate(
+                  (context, index) => Card30x20(),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 35,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text("Popular",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Container(
+              height: 240,
+              // height: double.maxFinite,
+              child: ListView.separated(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: 6,
+                itemBuilder: (context, index) => Card30x20(),
+                separatorBuilder: (BuildContext context, int index) {
+                  return SizedBox(width: 5);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
